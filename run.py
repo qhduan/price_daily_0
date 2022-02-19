09598946b9dc3331399a449c7aedb164698bebf9
qhduan <mail@qhduan.com>
2022-02-19 11:40:06 +0800

    1/10

--- run.py
+++ run.py
@@ -5,8 +5,9 @@ stock_zh_a_spot_em_df = ak.stock_zh_a_spot_em()
 codes = list(stock_zh_a_spot_em_df['代码'])
 os.makedirs('stocks', exist_ok=True)
 for code in codes:
-    print(code)
-    stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol=code, period="daily", adjust="qfq")
-    with open(f'stocks/{code}.csv', 'w') as fp:
-        fp.write(stock_zh_a_hist_df.to_csv(index=False))
+    if int(code) % 10 == 0:
+        print(code)
+	    stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol=code, period="daily", adjust="qfq")
+	    with open(f'stocks/{code}.csv', 'w') as fp:
+	        fp.write(stock_zh_a_hist_df.to_csv(index=False))
 
